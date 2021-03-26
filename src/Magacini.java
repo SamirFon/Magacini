dd8e969f7af7eaa194a089e6b6b1210ea6f57b63
SamirFon <samir.mehmedovic@fon.bg.ac.rs>
2021-03-26 11:31:39 +0100

    Napravljena klasa koja implementira metode iz interfejsa ali sa praznim telom

---
+++ src/Magacini.java
@@ -0,0 +1,25 @@
+import artikal.Artikli;
+import interfejs.MagacinInterfejs;
+
+public class Magacini  implements MagacinInterfejs{
+
+	@Override
+	public void dodavanjeArtikla(Artikli a) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void brisanjeArtikla(Artikli a, int kolicina) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String nadjiArtikal(int sifra) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+
+}
