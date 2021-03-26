197e45b18f3ce3056547a656d4bc6b9f51614fa3
SamirFon <samir.mehmedovic@fon.bg.ac.rs>
2021-03-26 11:37:10 +0100

    IMplementirane metode u klasi Magacini

--- src/Magacini.java
+++ src/Magacini.java
@@ -1,24 +1,42 @@
+import java.util.LinkedList;
+
 import artikal.Artikli;
 import interfejs.MagacinInterfejs;
 
 public class Magacini  implements MagacinInterfejs{
 
+	
+	LinkedList<Artikli> artikli = new LinkedList<Artikli>();
+
 	@Override
 	public void dodavanjeArtikla(Artikli a) {
-		// TODO Auto-generated method stub
+		for (int i=0; i<artikli.size(); i++)
+			if (artikli.contains(a))
+				throw new RuntimeException("Ovaj artikal se vec nalazi u listi");
+				artikli.add(a);
+			
 		
 	}
 
 	@Override
 	public void brisanjeArtikla(Artikli a, int kolicina) {
-		// TODO Auto-generated method stub
+		for (int i=0; i<artikli.size(); i++) {
+			if (artikli.contains(a)) 
+				a.setKolicina(a.getKolicina()-kolicina);
 		
+		}
 	}
 
 	@Override
 	public String nadjiArtikal(int sifra) {
-		// TODO Auto-generated method stub
-		return null;
+		String ar=null;
+		
+		for (int i=0; i<artikli.size(); i++) 
+			if (artikli.get(i).getSifra()==sifra) {
+			 ar = artikli.get(i).getNaziv();
+			}			
+		return ar;
+	
 	}
 	
 
